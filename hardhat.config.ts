04f4886a01f4c7a3c3274e794f738ee81419b7fb
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-19 13:29:08 +0530

    Ganache

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -23,7 +23,7 @@ export default {
       accounts: [process.env.PRIVATE_KEY],
     },
     ganache: {
-      url: "http://127.0.0.1:8545",
+      url: process.env.GANCHE_URL,
     },
     matic: {
       url: process.env.MATIC_URL,
