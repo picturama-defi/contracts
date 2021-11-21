6aefa11a0389f83fb5620789a07252faf15a1cf2
Jithin KS <jithinks@Jithins-MacBook-Pro.local>
2021-11-21 19:41:18 +0530

    id format change

--- test/filmOwnership.test.ts
+++ test/filmOwnership.test.ts
@@ -8,13 +8,27 @@ describe("Film ownership tests", function () {
     const RamaTokenFactory = await ethers.getContractFactory("RamaToken");
     const ramaTokenContract = await RamaTokenFactory.deploy();
 
-    await ramaTokenContract.addFilm(addr1.address, 1000);
-    await ramaTokenContract.addFilm(addr1.address, 2000);
+    await ramaTokenContract.addFilm(
+      addr1.address,
+      1000,
+      ethers.utils.formatBytes32String("id1")
+    );
+
+    await ramaTokenContract.addFilm(
+      addr1.address,
+      2000,
+      ethers.utils.formatBytes32String("id2")
+    );
 
     const res = await ramaTokenContract.getAllProjects();
 
-    expect(res[0].toString()).to.eq("1000,0,1");
-    expect(res[1].toString()).to.eq("2000,0,2");
+    expect(res[0]["targetAmount"].toString()).to.eq("1000");
+    expect(res[0]["totalFunded"].toString()).to.eq("0");
+    expect(ethers.utils.parseBytes32String(res[0]["id"])).to.eq("id1");
+
+    expect(res[1]["targetAmount"].toString()).to.eq("2000");
+    expect(res[1]["totalFunded"].toString()).to.eq("0");
+    expect(ethers.utils.parseBytes32String(res[1]["id"])).to.eq("id2");
   });
 
   it("tests addition of a film by user who is not the admin", async function () {
@@ -24,7 +38,9 @@ describe("Film ownership tests", function () {
     const ramaTokenContract = await RamaTokenFactory.deploy();
 
     await expect(
-      ramaTokenContract.connect(addr1).addFilm(addr1.address, 1000)
+      ramaTokenContract
+        .connect(addr1)
+        .addFilm(addr1.address, 1000, ethers.utils.formatBytes32String("id"))
     ).to.be.revertedWith("Unauthorised request");
   });
 
@@ -34,15 +50,17 @@ describe("Film ownership tests", function () {
     const RamaTokenFactory = await ethers.getContractFactory("RamaToken");
     const ramaTokenContract = await RamaTokenFactory.deploy();
 
-    ramaTokenContract.connect(owner).addFilm(addr1.address, 1000);
+    ramaTokenContract
+      .connect(owner)
+      .addFilm(addr1.address, 1000, ethers.utils.formatBytes32String("id"));
 
-    ramaTokenContract.fundFilm(1, {
+    ramaTokenContract.fundFilm(ethers.utils.formatBytes32String("id"), {
       value: 100,
     });
 
     const res = await ramaTokenContract.getAllProjects();
 
-    expect(res[0].toString()).to.eq("1000,100,1");
+    expect(res[0][1].toString()).to.eq("100");
   });
 
   it("tests over funding of a film", async function () {
@@ -51,16 +69,20 @@ describe("Film ownership tests", function () {
     const RamaTokenFactory = await ethers.getContractFactory("RamaToken");
     const ramaTokenContract = await RamaTokenFactory.deploy();
 
-    ramaTokenContract.connect(owner).addFilm(addr1.address, 1000);
+    ramaTokenContract
+      .connect(owner)
+      .addFilm(addr1.address, 1000, ethers.utils.formatBytes32String("id1"));
 
-    ramaTokenContract.fundFilm(1, {
+    ramaTokenContract.fundFilm(ethers.utils.formatBytes32String("id1"), {
       value: 100,
     });
 
     await expect(
-      ramaTokenContract.connect(addr1).fundFilm(1, {
-        value: 1000,
-      })
+      ramaTokenContract
+        .connect(addr1)
+        .fundFilm(ethers.utils.formatBytes32String("id1"), {
+          value: 1000,
+        })
     ).to.be.revertedWith("Excess fund");
   });
 });
